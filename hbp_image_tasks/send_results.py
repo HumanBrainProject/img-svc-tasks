55cb6c4e8467234fa76b6124c5edb4ed94f154ed
Akos Hencz <akos.hencz@epfl.ch>
2020-01-27 17:37:36 +0100

    Update Swift library settings

--- hbp_image_tasks/send_results.py
+++ hbp_image_tasks/send_results.py
@@ -8,12 +8,15 @@ from time import perf_counter
 from datetime import timedelta
 from swiftclient.multithreading import OutputManager
 from swiftclient.service import SwiftError, SwiftService, SwiftUploadObject
-
+from keystoneclient.v3 import client
+from keystoneauth1 import session
+from keystoneauth1.identity import v3
+from keystoneauth1.extras._saml2 import V3Saml2Password
 
 # S3 multithread
 # http://ls.pwd.io/2013/06/parallel-s3-uploads-using-boto-and-threads-in-python/
 
-CREDENTIALS_LOCATION = './.ec2_creds'
+SETTINGS_LOCATION = os.environ.get('TASK_SETTINGS', './.os_settings')
 
 logging.basicConfig(
     format="%(asctime)s %(levelname)s:%(name)s: %(message)s",
@@ -31,6 +34,26 @@ parser = argparse.ArgumentParser(description='Save results to Archival storage')
 parser.add_argument('results', help='The local path to the results directory')
 parser.add_argument('destination', help="The destination container")
 
+def get_keystone_token(settings):
+    # GETTING UNSCOPED TOKEN
+    # NEEDS LXML package
+    auth = V3Saml2Password(
+        identity_provider='cscskc',
+        protocol='mapped',
+        identity_provider_url='https://auth.cscs.ch/auth/realms/cscs/protocol/saml/',
+        auth_url=settings["os_auth_url"],
+        username=settings["os_username"],
+        password=settings["os_password"])
+    sess = session.Session(auth=auth)
+
+    # GETTING PROJECT SCOPED TOKEN USING PREVIOUS TOKEN
+    auth = v3.Token(
+        auth_url=settings["os_auth_url"],
+        token=sess.get_token(),
+        project_id=settings["os_project_id"])
+    sess = session.Session(auth=auth)
+    return sess.get_token()
+
 def convert_filenames_to_labels(results_folder, filepaths):
     '''
     Cut off results_folder from filename and  convert it into labels
@@ -41,30 +64,28 @@ def convert_filenames_to_labels(results_folder, filepaths):
     filenames = [path[len(results_folder):] for path in filepaths]
     pattern = re.compile(r'(\w+/\d+-\d+)/(\d+-\d+)/(\d+-\d+).gz$')
     label = r'\1_\2_\3'
-    import ipdb; ipdb.set_trace()
     return [re.sub(pattern, label, filename) for filename in filenames]
 
 def upload_results(results_folder, destination_container):
     # generate filelist from results folder
     files = [(os.path.join(dp, f)) for dp, dn, fn in os.walk(results_folder) for f in fn]
     labels = convert_filenames_to_labels(results_folder, files)
-    # with open(CREDENTIALS_LOCATION, 'r') as cf:
-    #     credentials = json.load(cf)
+    with open(SETTINGS_LOCATION, 'r') as sf:
+        settings = json.load(sf)
 
 
-    # https://docs.openstack.org/python-swiftclient/latest/service-api.html#upload
+    options = {'object_uu_threads': 20,
+               'os_auth_token': get_keystone_token(settings)}
+    del(settings['os_password'])
+    options.update(**settings)
 
-    # pollux env needs extra env vars:
-    # ST_AUTH_VERSION
-    # OS_AUTH_TOKEN (=OS_TOKEN)
-
-    _opts = {'object_uu_threads': 20}
-    with SwiftService(options=_opts) as swift, OutputManager() as out_manager:
+    with SwiftService(options=options) as swift, OutputManager() as out_manager:
         try:
             logger.info(f'Uploading {len(files)} objects to {destination_container}')
             start = perf_counter()
             objects = [SwiftUploadObject(file, object_name=labels[index]) for index, file in enumerate(files)]
             for result in swift.upload(destination_container, objects):
+                # import ipdb; ipdb.set_trace()
                 if not result['success']:
                     logger.error(f"Faield to uplad object {result['object']}")
             finish = perf_counter()
