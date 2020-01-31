bb3dac9938591699d9ed1ea66559e902bc278693
Akos Hencz <akos.hencz@epfl.ch>
2020-01-31 14:31:43 +0100

    Add cleanup and logging to wrapper

--- hbp_image_tasks/ingest.py
+++ hbp_image_tasks/ingest.py
@@ -1,11 +1,21 @@
 import argparse
-from os import getcwd
-from os.path import join
+import logging
+import sys
+from shutil import rmtree
+from os.path import join, isdir
 from .fetch_input import fetch_input
 from .ingest_ngs import ingest
 from .upload_results import upload_results
 
 
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s:%(name)s: %(message)s",
+    level=logging.INFO,
+    datefmt="%H:%M:%S",
+    stream=sys.stdout,
+)
+
+logger = logging.getLogger("ingest_wrapper")
 
 def main():
     parser = argparse.ArgumentParser(
@@ -37,28 +47,44 @@ def main():
 
     args = parser.parse_args()
     print(args)
-    fetch_input(
-        source=args.source,
-        destination=args.download_dir,
-        stacks=args.stacks,
-        filter=args.filter)
 
-    if not args.stacks:
-        #single file
-        ingest_input_path = join(args.download_dir, args.source.split('/')[-1])
-    else:
-        ingest_input_path = args.download_dir
+    try:
+        return_code = 0
+        fetch_input(
+            source=args.source,
+            destination=args.download_dir,
+            stacks=args.stacks,
+            filter=args.filter)
 
-    ingest(
-        path=ingest_input_path,
-        destination=args.results_dir,
-        parameters=args.parameters)
+        if not args.stacks:
+            #single file
+            ingest_input_path = join(args.download_dir, args.source.split('/')[-1])
+        else:
+            ingest_input_path = args.download_dir
 
-    if not args.noupload:
-        upload_results(
-            results_folder=args.results_dir,
-            destination_container=args.container,
-            cleanup=args.cleanup)
+        ingest(
+            path=ingest_input_path,
+            destination=args.results_dir,
+            parameters=args.parameters)
+
+        if not args.noupload:
+            upload_results(
+                results_folder=args.results_dir,
+                destination_container=args.container)
+    except Exception as exc:
+        return_code = 1
+    finally:
+        if args.cleanup:
+            if isdir(args.download_dir):
+                logger.info("Deleting download folder")
+                rmtree(args.download_dir)
+                logger.info("Done")
+            if isdir(args.results_dir):
+                logger.info("Deleting results folder")
+                rmtree(args.results_dir)
+                logger.info("Done")
+        sys.exit(return_code)
+
 
 if __name__ == '__main__':
     main()
